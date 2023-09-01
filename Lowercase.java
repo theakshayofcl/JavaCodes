473b19adc0cdfb0cb461945c3e9a340a3dfd7abd
theakshayofcl <achuakshay840@gmail.com>
2023-09-01 11:35:26 +0530

    New file added

---
+++ Lowercase.java
@@ -0,0 +1,6 @@
+public class Lowercase {
+    public static void main (String [] args){
+        String text = "GITHUB";
+        System.out.println(text.tolowerCase());
+    }
+}
---
+++ Uppercase.java
@@ -0,0 +1,6 @@
+public class Uppercase {
+    public static void main (String [] args){
+        String text = "github";
+        System.out.println(text.toUpperCase());
+    }
+}
